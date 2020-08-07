18ecd1d9533be43d7197bf24ce5b402c3a22ab57
Brian Engert <bengert@soliantconsulting.com>
2020-08-07 14:04:15 -0500

    support new lines in the question and subquestion text

--- src/components/QuestionYesNo.tsx
+++ src/components/QuestionYesNo.tsx
@@ -26,7 +26,7 @@ const QuestionYesNo = ({ question, setAnswerForQuestion}: Props) => {
         <>
             <Grid item xs={12} sm={9}>
                 <Typography className={classes.questionText} variant={'h5'} align={'left'}>{question.number}. {question.text}</Typography>
-                {question.subText && <Typography>{question.subText}</Typography>}
+                {question.subText && <Typography className={classes.questionSubText}>{question.subText}</Typography>}
             </Grid>
             <Grid item xs={12} sm={3} className={classes.buttons}>
                 <ButtonGroup color="primary" aria-label="outlined primary button group" fullWidth>
